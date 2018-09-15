c8e07ef299152c076587db54cb9d2bc6156673c4
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-15 11:01:49 -0300

    Creation of signup screen
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: André Lucas <andrelucax@gmail.com>

--- mobile/screens/SignUpScreen.js
+++ mobile/screens/SignUpScreen.js
@@ -1,25 +1,55 @@
-import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
-} from "react-native";
+import React, { Component } from 'react';
+import { TouchableOpacity, View, ActivityIndicator, Text, Alert} from 'react-native';
+import { AppRegistry, TextInput } from 'react-native';
+export default class App extends Component {
 
-class SignUpScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>SignUpScreen</Text>
-            </View>
-        );
-    }
+  constructor(props) {
+      super(props);
+      this.state = {email: '', password: ''};
+  }
+
+  handlePress = async () => {
+      fetch('http://192.168.43.75:8000/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        'email': this.state.email,
+        'password': this.state.password
+      }),
+  })
+  .then((response) => response.json())
+  .then((responseJson) => {
+   Alert.alert(responseJson.error);
+   //Alert.alert("Token:  " + responseJson.token);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render(){
+    return(
+     <View style={{paddingTop: 50, paddingLeft: 30 }}>
+     <Text> SignUp </Text>
+     <TextInput
+            style={{height: 40}}
+            placeholder="Insert Email"
+            onChangeText={(email) =>  this.setState({email})}
+      />
+      <TextInput
+             style={{height: 40}}
+             placeholder="Insert Password here"
+             secureTextEntry
+             onChangeText={(password) => this.setState({password})}
+       />
+
+      <TouchableOpacity onPress={this.handlePress.bind(this)}>
+       <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click me to register </Text>
+      </TouchableOpacity>
+
+  </View>
+    );
+  }
 }
-export default SignUpScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
