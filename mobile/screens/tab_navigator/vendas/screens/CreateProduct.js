b2d548ac81c3400c7f5981d35d8aa27dbb514951
Welison Lucas <welison.almeida.923@gmail.com>
2018-09-25 14:45:22 -0300

    Fitting layout to create product screen according to prototype.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- mobile/screens/tab_navigator/vendas/screens/CreateProduct.js
+++ mobile/screens/tab_navigator/vendas/screens/CreateProduct.js
@@ -5,8 +5,8 @@
 import React, { Component } from 'react';
 import {
     View,
-    Text,
-    StyleSheet
+    StyleSheet,
+    Text
 } from 'react-native';
 import ProductImage from '../components/ProductImage';
 import { Container, Header, Textarea, Content, Form, Item, Input, Label, Button } from 'native-base'
@@ -15,23 +15,25 @@ class CreateProduct extends Component {
     render() {
         return (
             <View style={styles.container}>
+              
               <ProductImage style= { styles.image } productImage='http://www.piniswiss.com/wp-content/uploads/2013/05/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef-300x199.png'>
               </ProductImage>
-                <Content>
-                  <Form>
-                    <Item floatingLabel>
-                      <Label>Titulo</Label>
-                      <Input />
-                    </Item>
-                    <Item floatingLabel>
-                      <Label>Preco</Label>
-                      <Input />
-                    </Item>
-                    <Textarea rowSpan={5} underline placeholder="Description" />
-                    <Button danger><Text> Danger </Text></Button>
-                    <Button success><Text> Success </Text></Button>
-                  </Form>
-                </Content>
+              <Form style={styles.description}>
+                <Item floatingLabel>
+                  <Label>Titulo</Label>
+                  <Input />
+                </Item>
+                <Item floatingLabel>
+                  <Label>Preco</Label>
+                  <Input />
+                </Item>
+                <Textarea rowSpan={2} underline placeholder="Description" />
+              </Form>
+              <View style={styles.buttonContainer}>
+                <Button style={{justifyContent: 'center', height: 40, width: 100}} danger><Text style={{color: 'white'}}> CANCELAR </Text></Button>
+                <Button style={{justifyContent: 'center', height: 40, width: 100}} success><Text style={{color: 'white'}}> SALVAR </Text></Button>
+              </View>
+              
             </View>
         );
     }
@@ -45,9 +47,16 @@ const styles = StyleSheet.create({
     },
     image: {
       width: '100%',
-      height: '45%'
+      height: '30%',
     },
     description: {
-      height: '55%'
-    }
+      flex: 1,
+      height: '35%',
+      width: '95%',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      paddingBottom: 10,
+    },
 });
