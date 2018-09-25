649532d421a3b3fcc17502fb749f09b3a08c94bd
zarbielli <jlfz06@gmail.com>
2018-09-25 13:39:45 -0300

    fga-eps-mds/2018.2-IndicaAi#53 - Adding SearchBar in IndicaAi.js and adding logo image
    Co-authored-by: Ridersk <lucasmacielaguiar@gmail.com>

--- mobile/screens/tab_navigator/indica_ai/IndicaAiApp.js
+++ mobile/screens/tab_navigator/indica_ai/IndicaAiApp.js
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  PixelRatio
 } from "react-native";
+import SearchBar from "./containers/searchBar";
+import { Provider } from "react-redux";
+
 
 class FirstScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Indica ai</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+          <Image
+            source={require("./images/logo.png")}
+            style={{
+              width: 317,
+              height: 115,
+              alignSelf: 'center'
+            }}
+          />
+          <Provider>
+            <SearchBar />
+          </Provider>
+      </View>
+    );
+  }
 }
 export default FirstScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  }
 });
