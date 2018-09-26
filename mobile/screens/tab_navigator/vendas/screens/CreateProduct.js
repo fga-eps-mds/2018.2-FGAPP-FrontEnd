526f04cc98976d80d2608407905544ea169182f0
Andre Lucas <andrelucax@gmail.com>
2018-09-25 23:48:07 -0300

    Integrated frontend with backend, form is now going to API Gateway. Changed text color on inputs to white.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- mobile/screens/tab_navigator/vendas/screens/CreateProduct.js
+++ mobile/screens/tab_navigator/vendas/screens/CreateProduct.js
@@ -5,33 +5,61 @@
 import React, { Component } from 'react';
 import {
     View,
-    Text,
-    StyleSheet
+    StyleSheet,
+    Text
 } from 'react-native';
 import ProductImage from '../components/ProductImage';
-import { Container, Header, Textarea, Content, Form, Item, Input, Label, Button } from 'native-base'
+import { Container, Header, Textarea, Content, Alert, Form, Item, Input, Label, Button } from 'native-base'
 
 class CreateProduct extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        title: '',
+        price: '',
+        description: '',
+      };
+    }
+
+    registerProduct = async () => {
+      fetch('http://'+ process.env.IP + ':5000/api/create_product/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'fk_vendor': '1',
+          'name': this.state.title,
+          'price': this.state.price,
+          'photo': 'https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg',
+          'description': this.state.description,
+        }),
+      })
+    }
+
+
     render() {
         return (
             <View style={styles.container}>
+
               <ProductImage style= { styles.image } productImage='http://www.piniswiss.com/wp-content/uploads/2013/05/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef-300x199.png'>
               </ProductImage>
-                <Content>
-                  <Form>
-                    <Item floatingLabel>
-                      <Label>Titulo</Label>
-                      <Input />
-                    </Item>
-                    <Item floatingLabel>
-                      <Label>Preco</Label>
-                      <Input />
-                    </Item>
-                    <Textarea rowSpan={5} underline placeholder="Description" />
-                    <Button danger><Text> Danger </Text></Button>
-                    <Button success><Text> Success </Text></Button>
-                  </Form>
-                </Content>
+              <Form style={styles.description}>
+                <Item floatingLabel>
+                  <Label>Titulo</Label>
+                  <Input style={{ color: "white" }} onChangeText={(title) => {this.setState({title})}} />
+                </Item>
+                <Item floatingLabel>
+                  <Label>Preco</Label>
+                  <Input style={{ color: "white" }} onChangeText={(price) => {this.setState({price})}} />
+                </Item>
+                <Textarea style={{ color: "white" }} onChangeText={(description) => {this.setState({description})}} rowSpan={2} underline placeholder="Description" />
+              </Form>
+              <View style={styles.buttonContainer}>
+                <Button onPress={() => {this.props.navigation.navigate('MyProducts');}} style={{justifyContent: 'center', height: 40, width: 100}} danger><Text style={{color: 'white'}}> CANCELAR </Text></Button>
+                <Button onPress={this.registerProduct} style={{justifyContent: 'center', height: 40, width: 100}} success><Text style={{color: 'white'}}> SALVAR </Text></Button>
+              </View>
+
             </View>
         );
     }
@@ -45,9 +73,16 @@ const styles = StyleSheet.create({
     },
     image: {
       width: '100%',
-      height: '45%'
+      height: '30%',
     },
     description: {
-      height: '55%'
-    }
+      flex: 1,
+      height: '35%',
+      width: '95%',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      paddingBottom: 10,
+    },
 });
