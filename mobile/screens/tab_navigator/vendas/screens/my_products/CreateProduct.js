319791873e56890efe39afa6e48766698a4a3446
Welison Lucas <welison.almeida.923@gmail.com>
2018-09-28 16:59:13 -0300

    Updating screen indentation and removing unecessary code.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- mobile/screens/tab_navigator/vendas/screens/my_products/CreateProduct.js
+++ mobile/screens/tab_navigator/vendas/screens/my_products/CreateProduct.js
@@ -9,7 +9,7 @@ import {
     Text
 } from 'react-native';
 import ProductImage from '../../components/ProductImage';
-import { Container, Header, Textarea, Content, Alert, Form, Item, Input, Label, Button } from 'native-base';
+import { Textarea, Form, Item, Input, Label, Button } from 'native-base';
 import jwt_decode from 'jwt-decode';
 import ErrorDialog from './ErrorDialog';
 
@@ -20,7 +20,7 @@ class CreateProduct extends Component {
         title: '',
         price: '',
         description: '',
-        isDialogVisible:false,
+        isDialogVisible: false,
         messageError: '',
       };
     }
@@ -46,7 +46,7 @@ class CreateProduct extends Component {
           'fk_vendor': user.user_id,
           'name': this.state.title,
           'price': this.state.price,
-          'photo': 'https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg',
+          'photo': 'https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg',
           'description': this.state.description,
         }),
       })
@@ -73,8 +73,7 @@ class CreateProduct extends Component {
     render() {
         const {state} = this.props.navigation;
         var token = state.params ? state.params.token : undefined;
-        var jwtDecode = require('jwt-decode');
-        var user = jwt_decode(token);
+
         return (
             <View style={styles.container}>
               <ErrorDialog
@@ -82,24 +81,50 @@ class CreateProduct extends Component {
                   isDialogVisible={this.state.isDialogVisible}
                   backButton = {this.closeDialog}
               />
-              <ProductImage style= { styles.image } photo='http://www.piniswiss.com/wp-content/uploads/2013/05/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef-300x199.png'>
+              <ProductImage
+                style= { styles.image }
+                photo='http://www.piniswiss.com/wp-content/uploads/2013/05/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef-300x199.png'>
               </ProductImage>
               <Form style={styles.description}>
                 <Item floatingLabel>
                   <Label>Título</Label>
-                  <Input style={{ color: "white" }} onChangeText={(title) => {this.setState({title})}} />
+                  <Input
+                    style={{ color: "white" }}
+                    onChangeText={(title) => {this.setState({title})}}
+                  />
                 </Item>
                 <Item floatingLabel>
                   <Label>Preço</Label>
-                  <Input style={{ color: "white" }} onChangeText={(price) => {this.setState({price})}} />
+                  <Input
+                    style={{ color: "white" }}
+                    keyboardType='numeric'
+                    onChangeText={(price) => {this.setState({price})}}
+                  />
                 </Item>
-                <Textarea style={{ color: "white" }} onChangeText={(description) => {this.setState({description})}} rowSpan={2} underline placeholder="Descrição" />
+                <Textarea
+                  style={{ color: "white" }}
+                  onChangeText={(description) => {this.setState({description})}}
+                  rowSpan={2}
+                  underline
+                  placeholder="Descrição"
+                />
               </Form>
               <View style={styles.buttonContainer}>
-                <Button onPress={() => {this.props.navigation.navigate('MyProducts', {token:token});}} style={{justifyContent: 'center', height: 40, width: 100}} danger><Text style={{color: 'white'}}> CANCELAR </Text></Button>
-                <Button onPress={this.registerProduct} style={{justifyContent: 'center', height: 40, width: 100}} success><Text style={{color: 'white'}}> SALVAR </Text></Button>
+                <Button
+                  onPress={() => {this.props.navigation.navigate('MyProducts', {token:token});}}
+                  style={styles.button}
+                  danger
+                >
+                  <Text style={{color: 'white'}}> CANCELAR </Text>
+                </Button>
+                <Button
+                  onPress={this.registerProduct}
+                  style={styles.button}
+                  success
+                >
+                  <Text style={{color: 'white'}}> SALVAR </Text>
+                </Button>
               </View>
-
             </View>
         );
     }
@@ -125,4 +150,9 @@ const styles = StyleSheet.create({
       justifyContent: 'space-around',
       paddingBottom: 10,
     },
+    button: {
+      justifyContent: 'center',
+      height: 40,
+      width: 100,
+    }
 });
