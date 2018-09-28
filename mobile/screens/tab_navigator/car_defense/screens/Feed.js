7b0c81d48a51fefc6e5b63c51589dee213e95e95
lievertom <lievertom@gmail.com>
2018-09-28 11:59:31 -0300

    Add Feed feature
    Co-authored-by: Paulo Vitor Rocha <paulovitorrocha.unb@gmail.com>
    Co-authored-by: Ivan Diniz <ivandinizdobbin@gmail.com>

--- mobile/screens/tab_navigator/car_defense/screens/Feed.js
+++ mobile/screens/tab_navigator/car_defense/screens/Feed.js
@@ -1,12 +1,83 @@
-import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View , StyleSheet } from 'react-native';
 
-export default class Feed extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Feed</Text>
-            </View>
-        );
+export default class FetchExample extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('http://www.mocky.io/v2/5bae33502e00004c00bb41a3')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
     }
+
+    return(
+      <View style={styles.item}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.item}>
+                <Text style={styles.text1}>{item.title}</Text>
+                <Text style={styles.text}>{item.message}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    flexGrow: 1,
+    margin: 4,
+    padding: 20,
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    },
+    elevation: 4
+  },
+  text: {
+    color: "#5c68c3"
+  },
+  text1: {
+    color: "#5c68c3",
+    textWeight: 400
+  }
+});
