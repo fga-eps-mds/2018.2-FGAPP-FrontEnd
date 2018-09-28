5c54e7c4ac3c8a73541ad5b19345dac656bb311e
bielrossi15 <bielrossiborba@gmail.com>
2018-09-28 17:53:14 -0300

    Adding function send private notifications
    Co-authored-by: João Gabriel Rossi <bielrossiborba@gmail.com>
    Co-authored-by: João Gabriel Antunes <jg.antunes.medeiros@gmail.com>

--- mobile/screens/tab_navigator/car_defense/screens/TabNavigator/PrivateNotifications/PrivateNotifications.js
+++ mobile/screens/tab_navigator/car_defense/screens/TabNavigator/PrivateNotifications/PrivateNotifications.js
@@ -7,42 +7,87 @@ import {
   TextInput,
   StyleSheet,
   Button,
-  Alert
+  Alert  
 } from 'react-native';
 
+
 export default class PrivateNotifications extends Component {
-    _onPressButton() {
-        Alert.alert('You tapped the button!')
-      }
+  
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      token: '',
+      title: 'Algo de errado no seu carro!',
+      message: ''
+    }
+  }
+
+  handlePlate = (text) => {
+    this.setState({id: text})
+  }
+
+  handleMessage = (text) => {
+      this.setState({title: text})   
+  }
+
+
+  onPressButton = event => {
+    const url = ``
+
+    let notification = JSON.stringify({
+      id: this.state.id,
+      title: this.state.title,
+      message: this.state.message
+    })
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: notification
+    }).then(response => { return response.json() }
+    ).then(jsonResponse => {
+      console.log(jsonResponse);
+    }
+    ).catch(error => {
+      console.log(error)
+    })
+
+  }
   render() {
     return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.header}>Informações</Text>
-        <Text style={styles.header2}> Usuário</Text>
-        <TextInput
-          style={styles.input2}
-          placeholderTextColor="#c8cdea"
-          placeholder="Digite o usuário"
-          underlineColorAndroid="transparent"
-        />
-        <Text style={styles.header3}>Descrição</Text>
-        <TextInput
-          style={styles.input1}
-          placeholderTextColor="#c8cdea"
-          placeholder="Descreva o ocorrido"
-          multiline={true}
-          maxLength={255}
-          underlineColorAndroid="transparent"
-        />
-      </View>
-      <View style={styles.container1}>
-         <Button title="Enviar" 
-            color="#5c68c3" 
-            onPress={this._onPressButton}
-         />
-      </View>
-    </ScrollView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.header}>Informações</Text>
+          <Text style={styles.header2}> Usuário</Text>
+          <TextInput
+            style={styles.input2}
+            placeholderTextColor="#c8cdea"
+            placeholder="Digite a placa"
+            underlineColorAndroid="transparent"
+            onChangeText={this.handlePlate}
+          />
+          <Text style={styles.header3}>Descrição</Text>
+          <TextInput
+            style={styles.input1}
+            placeholderTextColor="#c8cdea"
+            placeholder="Descreva o ocorrido"
+            multiline={true}
+            maxLength={255}
+            underlineColorAndroid="transparent"
+            onChangeText={this.handleMessage}
+          />
+        </View>
+        <View style={styles.container1}>
+          <Button title="Enviar"
+            color="#5c68c3"
+            onPress={this.onPressButton}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -93,6 +138,6 @@ const styles = StyleSheet.create({
     borderBottomColor: '#5c68c3',
     marginTop: 30,
   },
-  
+
 });
 
