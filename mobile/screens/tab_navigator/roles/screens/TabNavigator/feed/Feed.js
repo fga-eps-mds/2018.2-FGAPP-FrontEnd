e57399ddd8bb8f4d12fc81f11d783c9957233418
gleal17 <guilhermelml@gmail.com>
2018-09-28 23:39:26 -0300

    fga-eps-mds/2018.2-Roles#85 Fix font Robot bugs

--- mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/Feed.js
+++ mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/Feed.js
@@ -4,25 +4,29 @@ import {
     Card, CardItem, Text,
     Thumbnail, Left, Right  } from "native-base";
   
-  import React, { Component } from "react";
-  import { StyleSheet, Image } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Image } from "react-native";
+
 const logo=  require("./images/logo.png");
 const cardImage = require("./images/banner.png");
-class Feed extends Component {
+
+class Feed extends React.Component {   
     constructor(props) {
         super(props);
         this.state = { loading: true };
       }
-    
       async componentWillMount() {
-        await Font.loadAsync({
+        await Expo.Font.loadAsync({
           Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+          Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
         });
         this.setState({ loading: false });
       }
-    
     render() {
+        if(this.state.loading){
+            return <Expo.AppLoading />;
+        }
         return (
             <Container style ={StyleSheet.container}>
                 <Header>
