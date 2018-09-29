e061e6b9d369b334896cadcb36a78ea18291195c
gleal17 <guilhermelml@gmail.com>
2018-09-29 12:15:30 -0300

    fga-eps-mds/2018.2-Roles#85 Renderizar dados mockados

--- mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/FeedItem.js
+++ mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/FeedItem.js
@@ -3,67 +3,68 @@ import {
     Content, Button, Icon,
     Card, CardItem, Text,
     Thumbnail, Left, Right,
-  } from "native-base";
-  
+} from "native-base";
+
 import React, { Component } from "react";
 import { StyleSheet, Image, ScrollView } from "react-native";
 const logo = require("./images/logo.png");
 const cardImage = require("./images/banner.png");
 
-class FeedItem extends Component{
-    render (){
-        return(
-                            <Card style={styles.mb}>
-                            <CardItem>
-                                <Left>
-                                <Thumbnail source={logo} />
-                                <Body>
-                                    <Text>User 1</Text>
-                                    <Text note>Going to Rolê!</Text>
-                                </Body>
-                                </Left>
-                            </CardItem>
+class FeedItem extends Component {
+    render() {
 
-                            <CardItem cardBody>
-                                <Image
-                                style={{
-                                    resizeMode: "cover",
-                                    width: null,
-                                    height: 150,
-                                    flex: 1
-                                }}
-                                source={cardImage} />
-                            </CardItem>
+        return (
+            <Card style={styles.mb}>
+                <CardItem>
+                    <Left>
+                        <Thumbnail source={logo} />
+                        <Body>
+                            <Text>User 1</Text>
+                            <Text note>{this.props.nomeRole}</Text>
+                        </Body>
+                    </Left>
+                </CardItem>
 
-                            <CardItem style={{ paddingVertical: 0 }}>
-                                <Left>
-                                <Button transparent>
-                                    <Icon active name="thumbs-up" />
-                                    <Text>11 Likes</Text>
-                                </Button>
-                                </Left>
-                                <Body>
-                                <Button transparent>
-                                    <Icon active name="chatbubbles" />
-                                    <Text>89 Comments</Text>
-                                </Button>
-                                </Body>
-                                <Right>
-                                <Button transparent>
-                                    <Text> Read More</Text>
-                                </Button>
-                                </Right>
-                            </CardItem>
-                            </Card>
+                <CardItem cardBody>
+                    <Image
+                        style={{
+                            resizeMode: "cover",
+                            width: null,
+                            height: 150,
+                            flex: 1
+                        }}
+                        source={cardImage} />
+                </CardItem>
+
+                <CardItem style={{ paddingVertical: 0 }}>
+                    <Left>
+                        <Button transparent>
+                            <Icon active name="thumbs-up" />
+                            <Text>11 Likes</Text>
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Button transparent>
+                            <Icon active name="chatbubbles" />
+                            <Text>89 Comments</Text>
+                        </Button>
+                    </Body>
+                    <Right>
+                        <Button transparent>
+                            <Text> Read More</Text>
+                        </Button>
+                    </Right>
+                </CardItem>
+            </Card>
         );
     }
 }
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: "#FFF"
+        backgroundColor: "#FFF"
     },
     mb: {
-      marginBottom: 5
+        marginBottom: 5
     }
-  });
+});
 export default FeedItem;
