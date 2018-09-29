603bbb45e254676110665d64221566d390f7f1ae
bielrossi15 <bielrossiborba@gmail.com>
2018-09-29 17:31:21 -0300

    Refactoring car registration front-end functions
    Co-authored-by: João Gabriel Rossi <bielrossiborba@gmail.com>
    Co-authored-by: João Gabriel Antunes <jg.antunes.medeiros@gmail.com>

--- mobile/screens/tab_navigator/car_defense/screens/TabNavigator/RegisterCar/RegisterCar.js
+++ mobile/screens/tab_navigator/car_defense/screens/TabNavigator/RegisterCar/RegisterCar.js
@@ -1,118 +1,130 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, ScrollView, StyleSheet, TextInput, Button } from 'react-native';
-import Expo from 'expo';
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet, TextInput, Button, Alert } from 'react-native';
+import Expo from 'expo'
 
-async function getToken() {
-  // Remote notifications do not work in simulators, only on device
-  if (!Expo.Constants.isDevice) {
-    return;
-  }
-  let { status } = await Expo.Permissions.askAsync(
-    Expo.Permissions.NOTIFICATIONS,
+var tk
+
+async function register() {
+  const { status } = await Expo.Permissions.askAsync(
+    Expo.Permissions.NOTIFICATIONS
   );
-  if (status !== 'granted') {
+  if (status != 'granted') {
+    alert('You need to enable permissions in settings');
     return;
   }
-  let value = await Expo.Notifications.getExpoPushTokenAsync();
-  console.log('Our token', value);
-  /// Send this to a server
+
+  const token = await Expo.Notifications.getExpoPushTokenAsync();
+   tk = token;
+  console.log(status, token);
 }
 
+
 export default class RegisterCar extends Component {
-    componentDidMount () {
-        token = getToken()
+  componentWillMount() {
+    register();
+    this.listener = Expo.Notifications.addListener(this.listen);
+  }
+  componentWillUnmount() {
+    this.listener && Expo.Notifications.addListener(this.listen);
+  }
+
+  listen = ({ origin, data }) => {
+    console.log('cool data', origin, data);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      plate: '',
     }
-
-    constructor (props) {
-        super(props);
-        this.state = {
-          token: token,
-          carPlate: ''
-        }
-      }
-
-      handlePlate = (text) => {
-          this.setState({ carPlate: text })
-      }
-
-      onPressButton = () => {
-        const url = ``
-
-        let notification = JSON.stringify({
-          carPlate: this.state.carPlate
-        })
     
-        fetch(url, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: notification
-        }).then(response => { return response.json() }
-        ).then(jsonResponse => {
-          console.log(jsonResponse);
-        }
-        ).catch(error => {
-          console.log(error)
-        })
-    
-      }
+  }
+
+  handlePlate = (text) => {
+    this.setState({ plate: text })
+  }
 
 
-    render() {
-        return (
-            <ScrollView>
-                <View style={styles.container}>
-                    <Text style={styles.header}> Cadatrar carro</Text>
-                    <TextInput
-                        style={styles.input2}
-                        placeholderTextColor="#c8cdea"
-                        placeholder="Placa do carro"
-                        underlineColorAndroid="transparent"
-                        onChangeText={this.handlePlate}                    
-                    />
-                </View>
-                <View style={styles.container1}>
-                    <Button 
-                        title="Cadastrar"
-                        color="#5c68c3"
-                        onPress={this.onPressButton}
-                    />
-                </View>
-            </ScrollView>
-        );
+
+  onPressButton = () => {
+    const url = `http://192.168.0.4:8002/carprofiles/`//car profiles db models url
+
+    let notification = JSON.stringify({
+      notification_token: tk,
+      plate: this.state.plate,
+    })
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: notification
+    }).then(response => { return response.json() }
+    ).then(jsonResponse => {
+      console.log(jsonResponse);
     }
+    ).catch(error => {
+      console.log(error)
+    })
+
+  }
+
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.header}> Cadastrar carro</Text>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor="#c8cdea"
+            placeholder="Placa do carro"
+            underlineColorAndroid="transparent"
+            onChangeText={this.handlePlate}
+          />
+        </View>
+        <View style={styles.container1}>
+          <Button
+            title="Cadastrar"
+            color="#5c68c3"
+            onPress={this.onPressButton}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    container1: {
-      marginTop: 100
-    },
-    header: {
-      color: '#5c68c3',
-      textAlign: 'center',
-      fontWeight: '200',
-      fontSize: 50,
-      marginTop: 25
-    },
-    header2: {
-      color: '#5c68c3',
-      textAlign: 'left',
-      paddingLeft: 20,
-      paddingTop: 35,
-      fontWeight: '100',
-      fontSize: 30,
-    },
-    input: {
-      width: 300,
-      height: 30,
-      borderBottomWidth: 1,
-      alignSelf: 'center',
-      textAlign: 'left',
-      borderBottomColor: '#5c68c3',
-      marginTop: 30,
-    },
+  container: {},
+  container1: {
+    marginTop: 50
+  },
+  header: {
+    color: '#5c68c3',
+    textAlign: 'center',
+    fontWeight: '200',
+    fontSize: 50,
+    marginTop: 25
+  },
+  header2: {
+    color: '#5c68c3',
+    textAlign: 'left',
+    paddingLeft: 20,
+    paddingTop: 35,
+    fontWeight: '100',
+    fontSize: 30,
+  },
+  input: {
+    width: 300,
+    height: 30,
+    borderBottomWidth: 1,
+    alignSelf: 'center',
+    textAlign: 'left',
+    borderBottomColor: '#5c68c3',
+    marginTop: 70,
+  },
 
-  });
+});
