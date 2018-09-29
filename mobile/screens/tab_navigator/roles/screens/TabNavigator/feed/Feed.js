d49cbf49a9a8ed7b8cce151605b9d73743f2a89b
gleal17 <guilhermelml@gmail.com>
2018-09-28 23:03:17 -0300

    fga-eps-mds/2018.2-Roles#85 Scaffold layout for feed

--- mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/Feed.js
+++ mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/Feed.js
@@ -7,7 +7,7 @@ import {
   import React, { Component } from "react";
   import { StyleSheet, Image } from "react-native";
 
-export default class Feed extends Component {
+class Feed extends Component {
     render() {
         return (
             <Container style ={StyleSheet.container}>
@@ -16,13 +16,59 @@ export default class Feed extends Component {
                         <Text>Rolê 1 (nome do rolê)</Text>
                     </Body>
                 </Header>
+                <Content padder>
+            <Card style={styles.mb}>
+                <CardItem>
+                <Left>
+                    <Thumbnail source={logo.require("./images/logo.png")} />
+                    <Body>
+                    <Text>User 1</Text>
+                    <Text>Going to Rolê!</Text>
+                    </Body>
+                </Left>
+                </CardItem>
 
+                <CardItem cardBody>
+                <Image
+                    style={{
+                    resizeMode: "cover",
+                    width: null,
+                    height: 150,
+                    flex: 1
+                    }}
+                    source={cardImage.require("./images/banner.png")}
+                />
+                </CardItem>
 
-
-
+                <CardItem style={{ paddingVertical: 0 }}>
+                    <Left>
+                        <Button transparent>
+                        <Icon active name="thumbs-up" />
+                        <Text>11 Likes</Text>
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Button transparent>
+                        <Icon active name="chatbubbles" />
+                        <Text>89 Comments</Text>
+                        </Button>
+                    </Body>
+                    </CardItem>
+                </Card>
+            </Content>
             </Container>
                 
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "#FFF"
+    },
+    mb: {
+      marginBottom: 15
+    }
+  });
+
+export default Feed;
