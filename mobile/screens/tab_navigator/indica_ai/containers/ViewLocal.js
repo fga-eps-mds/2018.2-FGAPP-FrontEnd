a84ce7500cc35b3e0d6ab4f271690aab2be079f5
Heron Rodrigues <heronrsousa@hotmail.com>
2018-10-07 15:38:57 -0300

    fga-eps-mds/2018.2-IndicaAi#123 - Adding Icons library and styling ViewLocal page
    Co-authored-by: WeillerFernandes <weillerfernandes@gmail.com>

--- mobile/screens/tab_navigator/indica_ai/containers/ViewLocal.js
+++ mobile/screens/tab_navigator/indica_ai/containers/ViewLocal.js
@@ -3,9 +3,12 @@ import {
   View,
   Text,
   StyleSheet,
-  Image
+  Image,
+  ScrollView
 } from "react-native";
 import LocalMap from "../components/LocalMap.js";
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 export default class App extends Component{
   render() {
@@ -15,10 +18,43 @@ export default class App extends Component{
             source={require('../assets/fga.jpg')}
           />
           <View style={styles.info}>
-            <Text>FGA</Text>
+            <Text style={styles.localName}>
+              Universidade de Brasília - Faculdade do Gama [FGA]
+            </Text>
+            <Icon style={styles.localHeart}
+              name='ios-heart-outline'
+              color='black'
+              size={30}
+            />
+            <View style={styles.hr}></View>
             <View style={styles.localMap}>
               <LocalMap/>
             </View>
+            <View style={styles.hr}></View>
+            <Icon style={styles.localInfoIcons}
+              name='md-call'
+              color='black'
+              size={25}
+            />
+            <Icon style={styles.localInfoIcons}
+              name='md-pin'
+              color='black'
+              size={25}
+            />
+            <Icon style={styles.localInfoIcons}
+              name='md-clock'
+              color='black'
+              size={25}
+            />
+            <View style={styles.hr}></View>
+            <Text style={styles.localRating}>
+              Avaliação:
+            </Text>
+            <Icon style={styles.localInfoIcons}
+              name='md-star'
+              color='black'
+              size={25}
+            />
           </View>
         </View>
     );
@@ -28,6 +64,7 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     position:"absolute",
+    backgroundColor: "white",
     top:0,
     bottom:0,
     left:0,
@@ -38,8 +75,33 @@ const styles = StyleSheet.create({
     top: 230,
     padding: 10,
   },
+  hr:{
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: 330,
+    left: 4.5,
+    marginVertical: 10
+  },
+  localName:{
+    width: 250,
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft: 15,
+    marginBottom: -25,
+  },
+  localHeart:{
+    marginLeft: 285,
+    top: -15
+  },
   localMap:{
     height: 180,
     width: 340
+  },
+  localInfoIcons:{
+    left: 20
+  },
+  localRating:{
+    left: 20,
+    fontSize: 16
   }
 });
