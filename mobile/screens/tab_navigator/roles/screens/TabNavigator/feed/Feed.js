b0718a4975be7e923dd9ce4df8b2901e72ea0421
Kalliu Yuri Silva Brasil <kalliub@gmail.com>
2018-10-09 15:13:55 -0300

    fga-eps-mds/2018.2-Roles#85 Added Pull to Refresh functionality
    Co-Authored-By: kalliub <kalliub@gmail.com>
    Co-Authored-By: gleal17 <guilhermelml@gmail.com>

--- mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/Feed.js
+++ mobile/screens/tab_navigator/roles/screens/TabNavigator/feed/Feed.js
@@ -6,7 +6,7 @@ import {
 } from "native-base";
 
 import React, { Component } from "react";
-import { View, StyleSheet, ScrollView, } from "react-native";
+import { View, StyleSheet, ScrollView, RefreshControl, } from "react-native";
 import FeedItem from "./FeedItem";
 class Feed extends Component {
   async componentWillMount() {
@@ -16,29 +16,29 @@ class Feed extends Component {
       Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
     });
     this.setState({ loading: false });
+    this._refreshFeed();
   }
 
   state = {
     loading: true,
     roles: [],
+    refreshing: false,
   };
 
-  _refreshFunc(){
+  _refreshFeed = () => {
+    this.setState({refreshing: true})
     fetch('http://209.97.153.172:8002/events/')
     .then(res => res.json())
     .then(roles =>{
       this.setState({ loading: false, roles })
       })
+    .then( () => {this.setState({refreshing: false}) })
     .catch((error) => {
       this.setState({
         loading: false
       });
       console.error(error);
     });
-  }
-
-  componentDidMount() {
-   this._refreshFunc()
   }
 
   render() {
@@ -49,18 +49,15 @@ class Feed extends Component {
     return (
       <View>
 
-        <ScrollView style={styles.scroll}>
-          <Button
-            rounded 
-            full 
-            success 
-            iconLeft 
-            onPress={()=>this._refreshFunc()} 
-            style={{marginBottom: 15}}
-          >
-            <Icon name="refresh"/>
-            <Text>Recarregar Eventos</Text>
-          </Button>
+        <ScrollView style={styles.scroll}
+          refreshControl={
+            <RefreshControl
+              refreshing = {this.state.refreshing}
+              onRefresh = {this._refreshFeed}
+            />
+          }
+        >
+
           {
             this.state.roles.map((role,index) => <FeedItem key={index} imgRole={role.photo} nomeRole={role.event_name} org={role.owner} />)
           }
