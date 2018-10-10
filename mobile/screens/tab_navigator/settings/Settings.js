0bb136ab1b9666bc8756b7907c3061d22c99ce5c
Welison Lucas <welison.almeida.923@gmail.com>
2018-10-10 03:53:57 -0300

    Creating card with user informations using native-base components.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

--- mobile/screens/tab_navigator/settings/Settings.js
+++ mobile/screens/tab_navigator/settings/Settings.js
@@ -7,6 +7,17 @@ import {
     Alert,
     Image,
 } from "react-native";
+import {
+  List,
+  ListItem,
+  Thumbnail,
+  Content,
+  Body,
+  Container,
+  Left,
+  Right,
+  Icon
+} from 'native-base';
 import {StackNavigator, createStackNavigator} from 'react-navigation'
 import UserCard from './../../components/UserCard'
 import jwt_decode from 'jwt-decode'
@@ -51,22 +62,28 @@ _onPressButton = async () => {
     var jwtDecode = require('jwt-decode');
     var user = jwt_decode(token);
       return (
-        <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between', paddingBottom:20, paddingTop:10}}>
-          <View>
-            <UserCard
-              imageSource = {{uri: 'https://www.logolynx.com/images/logolynx/03/039b004617d1ef43cf1769aae45d6ea2.png'}}
-              namePlaceholder = {""}
-              emailPlaceholder = {user.email}
-            />
-          </View>
-          <View style={{paddingLeft: '35%', paddingRight: '35%'}}>
-            <Button
-              color = "#BD1C5F"
-                  onPress={this._onPressButton}
-                  title="Sair"
-            />
-          </View>
-        </View>
+        <Container style= {{backgroundColor: "transparent", opacity: 1}}>
+          <Content>
+            <List>
+              <ListItem noIndent style={{paddingTop: 20, paddingLeft: '5%', paddingRight: '5%', paddingBottom: 20, borderBottomWidth:0, backgroundColor: 'white'}} >
+                <Left style={{alignItems: 'center'}}>
+                  <Thumbnail large source={{uri: 'https://www.logolynx.com/images/logolynx/03/039b004617d1ef43cf1769aae45d6ea2.png'}}/>
+                  <Body>
+                    <Text style={{fontSize: 20, fontWeight: '500'}}>
+                      Name
+                    </Text>
+                    <Text style={{fontSize: 15}}>
+                      Email
+                    </Text>
+                  </Body>
+                </Left>
+                <Right>
+                    <Icon style={{fontSize: 25}} name="arrow-forward" />
+                </Right>
+              </ListItem>
+            </List>
+          </Content>
+        </Container>              
       );
   }
 }
