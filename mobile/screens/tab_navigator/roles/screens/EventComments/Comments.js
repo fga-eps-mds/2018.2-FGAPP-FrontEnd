7f6b998cd4251141cc780fbef944cbf467ac535f
Victor Rodrigues <manovictor5@gmail.com>
2018-10-18 21:17:37 -0300

    fga-eps-mds/2018.2-Roles#98 Removando BUG de loading infinito

--- mobile/screens/tab_navigator/roles/screens/EventComments/Comments.js
+++ mobile/screens/tab_navigator/roles/screens/EventComments/Comments.js
@@ -1,13 +1,63 @@
 import React, { Component } from "react"
-import {Card, CardItem} from "native-base"
-import { View, Text } from "react-native"
+import {Card, CardItem, Left, Thumbnail, Body} from "native-base"
+import { View, Text, FlatList } from "react-native"
+
+const noPic = require("../TabNavigator/feed/images/noPic.png")
 
 class Comments extends Component {
+    state = {
+        loading: true,
+        comment: ""
+    }
+
+    _getDadosRole = () => {
+        fetch("https://5bc7da788bfe5a00131b6e6d.mockapi.io/comments/")
+            .then(res => res.json())
+            .then(resJson => {
+                this.setState({ loading: false,  comment: resJson})
+                console.log(resJson)
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false
+                })
+                console.error(error)
+            })
+    }
+
+    componentDidMount() {
+       this._getDadosRole()
+    }
+
     render() {
+        const {comment} = this.state
+        if(this.state.loading) {
+            return (
+                <View>
+                    <Text>
+                        Carregando...
+                        {this.state.comment}
+                    </Text>
+                </View>
+            )
+        }
         return (
             <Card>
                 <Text>Comentarios.</Text>
                 <CardItem>
+                <Left>
+                    <Thumbnail
+                        source={noPic}
+                    />
+                </Left>
+                <Body>
+                    <Text>
+                        Lista de comentários aqui.
+                        {
+                            //this.state.comment
+                        }
+                    </Text>
+                </Body>
 
                 </CardItem>
             </Card>
---
+++ mobile/screens/tab_navigator/roles/screens/EventComments/components/CommentItem.js
@@ -0,0 +1,26 @@
+import {
+	Body,
+	Button,
+	Icon,
+	Card,
+	CardItem,
+	Text,
+	Thumbnail,
+	Left,
+	Right,
+	Row,
+	Item
+} from "native-base"
+import React, { Component } from "react"
+import { StyleSheet, Image, View, TouchableOpacity } from "react-native"
+
+class CommentItem extends Component {
+
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
