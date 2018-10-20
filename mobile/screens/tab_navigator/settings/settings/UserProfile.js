d7a54e3c8a7fd8cd7512bb3507871a628d29cfbb
Welison Lucas <welison.almeida.923@gmail.com>
2018-10-20 01:34:56 -0300

    Creating method to upload image and change user informations on API.
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>

--- mobile/screens/tab_navigator/settings/settings/UserProfile.js
+++ mobile/screens/tab_navigator/settings/settings/UserProfile.js
@@ -8,53 +8,80 @@ import {
   TouchableOpacity,
   Alert,
 } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Body, Text, Form, Item , Label, Input } from 'native-base';
+import { Container, Header, Content, Card, CardItem, Body, Text, Form, Item, Label, Input } from 'native-base';
+import jwt_decode from 'jwt-decode'
 
 class UserProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user_id: 0,
-      name: '',
-      email: '',
-      photo: '',
+      user_id: 1,
+      name: undefined,
+      email: undefined,
+      photo: undefined,
     };
   }
 
   _clickPhoto = async () => {
-    Alert.alert('Voce Clicou na foto. Parabéns');
+    const {
+      cancelled,
+      uri,
+    } = await Expo.ImagePicker.launchImageLibraryAsync({ mediaTypes: 'Images', quality: 0.7, base64: true });
+    if (!cancelled) {
+      this.setState({ photo: uri });
+    }
   }
   _editProfile = async () => {
-    const logout_path = `${process.env.INTEGRA_LOGIN_AUTH}/api/logout/`;
-    fetch(logout_path, {
+    const { state } = this.props.navigation;
+    var token = state.params ? state.params.token : undefined;
+    var userInfo = state.params ? state.params.userInfo : undefined;
+    var user = jwt_decode(token);
+
+    var name = (this.state.name == undefined) ? userInfo.name : this.state.name;
+    var email = (this.state.email == undefined) ? userInfo.email : this.state.email;
+
+    const uri = (this.state.photo == undefined) ? userInfo.photo : this.state.photo;
+    const uriParts = uri.split('.');
+    const fileType = uriParts[uriParts.length - 1];
+    const formData = new FormData();
+
+    const apiUrl = `${process.env.INTEGRA_LOGIN_AUTH}/api/users/update_profile/`;
+    formData.append('user_id', user.user_id);
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('photo', {
+      uri,
+      name: `photo.${fileType}`,
+      type: `application/${fileType}`,
+    });
+
+    const options = {
       method: 'POST',
+      body: formData,
       headers: {
-        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data',
       },
-      body: JSON.stringify({
-        'user_id': this.state.user_id,
-        'name': this.state.name,
-        'email': this.state.email,
-        'photo': this.state.photo,
-      }),
-    })
+    };
+    fetch(apiUrl, options)
     .then((response) => {
       return response.json();
     })
     .then((responseJson) => {
-      //Json retorna com erro
+      // Json retorna com erro
       if (responseJson.error != undefined){
         Alert.alert(responseJson.error);
       }
-      //Json retorna sem erro
+      // Json retorna sem erro
       else{
-        Alert.alert('Json sem erro');
+        Alert.alert('Informações atualizadas com sucesso.');
       }
     })
     .catch((err) => {
       Alert.alert('Erro interno, não foi possível se comunicar com o servidor.');
     })
   }
+
   _logout = async () => {
     const logout_path = `${process.env.INTEGRA_LOGIN_AUTH}/api/logout/`;
     fetch(logout_path, {
@@ -94,35 +121,35 @@ class UserProfile extends Component {
 
     return (
       <View style={styles.container}>
-        <View style={{margin:5}}>
-          <Card style={{height:150, paddingRight:10}}>
-            <CardItem style={{height:'100%'}}>
+        <View style={{ margin: 5 }}>
+          <Card style={{ height: 150, paddingRight: 10 }}>
+            <CardItem style={{ height: '100%' }}>
               <Body>
-                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
                   <TouchableOpacity onPress={this._clickPhoto} style={styles.view_circle}>
                     <View>
                       <Image
                         source={{ uri: 'http://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg' }}
-                        style={{width: 100, height: 100, borderRadius:100, position: 'absolute'}}
+                        style={{ width: 100, height: 100, borderRadius: 100, position: 'absolute' }}
                       />
                       <Image
-                        source={{uri: 'https://i.imgur.com/gr7Zvft.png'}}
+                        source={{ uri: 'https://i.imgur.com/gr7Zvft.png' }}
                         style={styles.image_circle}
                       />
                     </View>
                   </TouchableOpacity>
                   <View>
                     <Item stackedLabel>
-                      <Label style={{fontSize: 12}}>Nome:</Label>
+                      <Label style={{ fontSize: 12 }}>Nome:</Label>
                       <Input
-                        style={{fontSize: 12}}
+                        style={{ fontSize: 12 }}
                         placeholder={'Nome'}
                       />
                     </Item>
                     <Item stackedLabel>
-                      <Label style={{fontSize: 12}}>Email</Label>
+                      <Label style={{ fontSize: 12 }}>Email</Label>
                       <Input
-                        style={{fontSize: 12}}
+                        style={{ fontSize: 12 }}
                         placeholder={'email@email.com'}
                       />
                     </Item>
@@ -132,14 +159,14 @@ class UserProfile extends Component {
             </CardItem>
           </Card>
         </View>
-        <View style={{margin:5}}>
+        <View style={{ margin: 5 }}>
           <Button
             color='#BD1C5F'
             onPress={this._editProfile}
             title="Salvar"
           />
         </View>
-        <View style={{margin:5}}>
+        <View style={{ margin: 5 }}>
           <Button
             color='#BD1C5F'
             onPress={this._logout}
@@ -163,23 +190,23 @@ const styles = StyleSheet.create({
     position: 'relative'
   },
   image_circle: {
-    borderWidth:1,
-    borderColor:'rgba(0,0,0,0.2)',
-    alignItems:'center',
-    justifyContent:'center',
-    width:100,
-    height:100,
-    borderRadius:100,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
     //backgroundColor:'transparent',
   },
   view_circle: {
-    alignItems:'center',
-    justifyContent:'center',
-    width:100,
-    height:100,
-    borderRadius:10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 10,
     borderWidth: 1,
-    borderColor:'rgba(0,0,0,0.2)',
-    borderRadius: 100/2,
+    borderColor: 'rgba(0,0,0,0.2)',
+    borderRadius: 100 / 2,
   },
 });
