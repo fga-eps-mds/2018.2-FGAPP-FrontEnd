dcf5cf958837689acc68a442ab9439dc3be013c3
Kalliu Yuri Silva Brasil <kalliub@gmail.com>
2018-10-22 02:30:42 -0300

    fga-eps-mds/2018.2-Roles#98 Created photo Modal inside Event Profile

--- mobile/screens/tab_navigator/roles/screens/EventProfile/components/Geral.js
+++ mobile/screens/tab_navigator/roles/screens/EventProfile/components/Geral.js
@@ -11,85 +11,137 @@ import {
 	List,
 	ListItem
 } from "native-base"
-import { Text, StyleSheet } from "react-native"
-import Divider from './Divider'
-import CardHeader from './CardHeader'
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	Image,
+	Alert
+} from "react-native"
+import Divider from "./Divider"
+import CardHeader from "./CardHeader"
+import PicModal from "./PicModal"
+import Modal from "react-native-modal"
+import ImageView from "react-native-image-view"
 
 const adultOnly = require("../../../static/adultOnly.png")
 const noPic = require("../../../static/noPic.png")
 
 class Geral extends Component {
+	state = {
+		picModalVisible: false
+	}
+
+	_toggleModal = () => {
+		this.props.photo != null
+			? this.setState({ picModalVisible: !this.state.picModalVisible })
+			: Alert.alert("Aviso", "Não há foto disponível para este rolê.")
+	}
+
 	render() {
 		const uri = this.props.photo
-		const dataFormatada = this.props.eventDate.slice(-2) + '/' + this.props.eventDate.slice(5,7) + '/' + this.props.eventDate.slice(0,4)
+		const dataFormatada =
+			this.props.eventDate.slice(-2) +
+			"/" +
+			this.props.eventDate.slice(5, 7) +
+			"/" +
+			this.props.eventDate.slice(0, 4)
+		const images = [
+			{
+				source: { uri: uri },
+				title: "Foto do Rolê"
+			}
+		]
 		return (
-			<Card>
-				<CardHeader text="Geral"/>
-				<CardItem>
-					<Left>
-						<Thumbnail
-							style={styles.thumbnailGeral}
-							source={this.props.photo == null ? noPic : {uri: uri}}
-						/>
-					</Left>
+			<View>
+				<ImageView
+					images={images}
+					imageIndex={0}
+					isVisible={this.state.picModalVisible}
+					animationType="fade"
+				/>
 
-					<Body>
-						<H2
-							style={{
-								textAlign: "center",
-								alignSelf: "center"
-							}}
-						>
-							{this.props.eventName}
-						</H2>
-
-						<Divider/>
-
-						<Card style={styles.descCards}>
-							<Left>
-								<Icon name="time" />
-							</Left>
-							<Right>
-								<Text>{this.props.eventHour.slice(0,5)}h</Text>
-							</Right>
-						</Card>
-
-						<Card style={styles.descCards}>
-							<Left>
-								<Icon name="calendar" />
-							</Left>
-							<Right>
-								<Text>{dataFormatada}</Text>
-							</Right>
-						</Card>
-
-						<Card style={styles.descCards}>
-							<Left>
-								<Icon name="cash" />
-							</Left>
-							<Right>
-								<Text>R$ {this.props.value}</Text>
-							</Right>
-						</Card>
-					</Body>
-				</CardItem>
-
-				{/* Sessão do +18; Se for true, ele mostra o card dizendo que a classificação indicativa é +18 */}
-				{this.props.adultOnly == true && (
-					<Card transparent>
-						<Body>
-							<CardItem>
-								<Text>Classificação Indicativa:</Text>
+				<Card>
+					<CardHeader text="Geral" />
+					<CardItem>
+						<Left>
+							<TouchableOpacity
+								onPress={() => {
+									this._toggleModal()
+								}}
+							>
 								<Thumbnail
-									style={{ marginLeft: 20 }}
-									small
-									source={adultOnly}
+									style={styles.thumbnailGeral}
+									source={
+										this.props.photo == null
+											? noPic
+											: { uri: uri }
+									}
 								/>
-							</CardItem>
+							</TouchableOpacity>
+						</Left>
+
+						<Body>
+							<H2
+								style={{
+									textAlign: "center",
+									alignSelf: "center"
+								}}
+							>
+								{this.props.eventName}
+							</H2>
+
+							<Divider />
+
+							<Card style={styles.descCards}>
+								<Left>
+									<Icon name="time" />
+								</Left>
+								<Right>
+									<Text>
+										{this.props.eventHour.slice(0, 5)}h
+									</Text>
+								</Right>
+							</Card>
+
+							<Card style={styles.descCards}>
+								<Left>
+									<Icon name="calendar" />
+								</Left>
+								<Right>
+									<Text>{dataFormatada}</Text>
+								</Right>
+							</Card>
+
+							<Card style={styles.descCards}>
+								<Left>
+									<Icon name="cash" />
+								</Left>
+								<Right>
+									<Text>R$ {this.props.value}</Text>
+								</Right>
+							</Card>
 						</Body>
-					</Card>
-				)}
-			</Card>
+					</CardItem>
+
+					{/* Sessão do +18; Se for true, ele mostra o card dizendo que a classificação indicativa é +18 */}
+					{this.props.adultOnly == true && (
+						<Card transparent>
+							<Body>
+								<CardItem>
+									<Text>Classificação Indicativa:</Text>
+									<Thumbnail
+										style={{ marginLeft: 20 }}
+										small
+										source={adultOnly}
+									/>
+								</CardItem>
+							</Body>
+						</Card>
+					)}
+				</Card>
+			</View>
 		)
 	}
 }
@@ -104,6 +156,11 @@ const styles = StyleSheet.create({
 		borderRadius: 125,
 		width: 140,
 		height: 140
+	},
+	footerPhotoModal: {
+		// color: "white",
+		width: "100%",
+		textAlign: "center"
 	}
 })
 
---
+++ mobile/screens/tab_navigator/roles/screens/EventProfile/components/PicModal.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {View, StyleSheet} from 'react-native'
+import {Image} from 'native-base'
+import Modal from 'react-native-modal'
+
+const noPic = require('../../../static/noPic.png')
+
+export default class PicModal extends Component{
+	render(){
+		return(
+			<View>
+				
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	img: {
+		flex: 1,
+		height: '90%',
+		width: '90%'
+	}
+})
