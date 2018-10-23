d8544d7d1ad0fd4a43bfaeede0d19608554d98db
Kalliu Yuri Silva Brasil <kalliub@gmail.com>
2018-10-23 00:36:30 -0300

    fga-eps-mds/2018.2-Roles#100 Making Maps work fully on 'Localização' section card

--- mobile/screens/tab_navigator/roles/screens/EventProfile/components/Localizacao.js
+++ mobile/screens/tab_navigator/roles/screens/EventProfile/components/Localizacao.js
@@ -1,44 +1,192 @@
 import React, { Component } from "react"
-import { Card, CardItem, Icon } from "native-base"
-import { View, Text, StyleSheet } from "react-native"
-import CardHeader from './CardHeader'
+import { Card, CardItem, Icon, Button } from "native-base"
+import { View, ScrollView, Text, StyleSheet } from "react-native"
+import { MapView, Location, Permissions } from "expo"
+import { Marker, Circle } from "react-native-maps"
+
+import CardHeader from "./CardHeader"
 
 class Localizacao extends Component {
+	static navigationOptions = {
+		title: "<MapView />"
+	}
+
+	state = {
+		enderecoRole: "",
+		region: {
+			latitude: 0,
+			longitude: 0,
+			latitudeDelta: 0.003,
+			longitudeDelta: 0.003
+		},
+		coordenadaRole: {
+			latitude: -15.9895303,
+			longitude: -48.0469959,
+			latitudeDelta: 0.004,
+			longitudeDelta: 0.004
+		},
+		alignLocalBtn: false
+	}
+
+	componentDidMount() {
+		Permissions.askAsync(Permissions.LOCATION)
+		this._getAddressFromCoordinates()
+		this.setState({ region: this.state.coordenadaRole })
+	}
+
+	_onRegionChange(region) {
+		this.setState({ region })
+	}
+
+	_getAddressFromCoordinates = async () => {
+		try {
+			let result = await Location.reverseGeocodeAsync({
+				latitude: this.state.coordenadaRole.latitude,
+				longitude: this.state.coordenadaRole.longitude
+			})
+			this.setState({ enderecoRole: result[0] })
+		} catch (e) {
+			this.setState({ error: e })
+		}
+	}
+
+	// Isso aqui é só se precisar
+	_getLocalizationFromAddress = async () => {
+		try {
+			let result = await Location.geocodeAsync("72120-970")
+			this.setState({
+				region: {
+					latitude: result[0].latitude,
+					longitude: result[0].longitude,
+					latitudeDelta: this.state.region.latitudeDelta,
+					longitudeDelta: this.state.region.longitudeDelta
+				}
+			})
+		} catch (e) {
+			this.setState({ error: e })
+		}
+	}
+
 	render() {
+		const { enderecoRole } = this.state
+
 		return (
-			<Card>
-				<CardHeader text="Localização"/>
+			<View>
+				<Card>
+					<CardHeader text="Localização" />
 
-				<CardItem>
-					<Icon name="pin" style={{ fontSize: 50, color:"black" }} />
+					<CardItem>
+						<Icon name="pin" style={styles.pinIcon} />
 
-					<Card
-						transparent
-						style={{ width: 250, marginLeft: 20 }}
-					>
-						<Text style={{ fontWeight: "bold" }}>
-							{this.props.address}
-						</Text>
-						<Text style={{ color: "grey" }}>Endereço da localização gerada pelo Maps</Text>
-					</Card>
-				</CardItem>
+						<View style={{ width: "100%", marginLeft: 20 }}>
+							<Text style={{ fontWeight: "bold" }}>
+								{this.props.address}
+							</Text>
+							<Text style={{ color: "grey" }}>
+								{enderecoRole.city != null &&
+									enderecoRole.city + ", "}
+								{enderecoRole.street != null &&
+									enderecoRole.street + ", "}
+								{enderecoRole.name != null &&
+									enderecoRole.name + "\n"}
+								{enderecoRole.region != null &&
+									enderecoRole.region + " - "}
+								{enderecoRole.country != null &&
+									enderecoRole.country + "\n"}
+								{enderecoRole.postalCode != null &&
+									enderecoRole.postalCode + "\n"}
+							</Text>
+						</View>
+					</CardItem>
 
-				<CardItem style={{alignSelf:'center'}}>
-					<View
-						style={{
-							backgroundColor: "grey",
-							width: 300,
-							height: 200,
-							justifyContent: 'center',
+					{this.state.region.latitude != 0 && (
+						<ScrollView style={styles.mapView}>
+							<MapView
+								ref={ref => {
+									this._mapView = ref
+								}}
+								style={{
+									width: "100%",
+									height: 250
+								}}
+								region={this.state.region}
+								initialRegion={this.state.coordenadaRole}
+								onRegionChangeComplete={region =>
+									this._onRegionChange(region)
+								}
+								loadingEnabled={true}
+							>
+								<Marker
+									coordinate={{
+										latitude: this.state.coordenadaRole
+											.latitude,
+										longitude: this.state.coordenadaRole
+											.longitude
+									}}
+									title={this.props.address}
+									description={"Local do Rolê"}
+								/>
+								<Circle
+									center={{
+										latitude: this.state.coordenadaRole
+											.latitude,
+										longitude: this.state.coordenadaRole
+											.longitude
+									}}
+									radius={20}
+									strokeWidth={2}
+									strokeColor='green'
+								/>
+							</MapView>
+						</ScrollView>
+					)}
+					<Text style={styles.coordinates}>
+						Latitude: {this.state.region.latitude} / Longitude:{" "}
+						{this.state.region.longitude}
+					</Text>
+
+					<Button
+						onPress={() => {
+							this.setState({ region: this.state.coordenadaRole })
 						}}
+						block
+						style={styles.centralizeBtn}
 					>
-						<Text style={{textAlign:'center', color:'white'}}>GOOGLE MAPS API {"\n"}
-						Link Address: {this.props.linkAddress}</Text>
-					</View>
-				</CardItem>
-			</Card>
+						<Icon name='locate' style={{color: 'black'}}/>
+						<Text> Realinhar Localização</Text>
+					</Button>
+				</Card>
+			</View>
 		)
 	}
 }
 
 export default Localizacao
+
+const styles = StyleSheet.create({
+	mapView: {
+		borderWidth: 1,
+		borderColor: "grey",
+		width: "95%",
+		alignSelf: "center"
+	},
+	coordinates: {
+		color: "grey",
+		textAlign: "center",
+		alignSelf: "center",
+		fontSize: 10,
+		marginBottom: 10
+	},
+	pinIcon: {
+		fontSize: 50,
+		color: "black"
+	},
+	centralizeBtn: {
+		marginBottom: 10,
+		alignSelf: "center",
+		width: "95%",
+		backgroundColor: 'white',
+		borderWidth: 1,
+		borderColor: 'black'
+	}
+})
