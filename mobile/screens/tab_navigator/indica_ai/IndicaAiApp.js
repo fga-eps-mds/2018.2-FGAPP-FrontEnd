7c48dd29b68cd41078eeffeab1f36f37cbc4deeb
Heron Rodrigues <heronrsousa@hotmail.com>
2018-10-26 09:19:18 -0300

    fga-eps-mds/2018.2-IndicaAi#127 - Adding form

--- mobile/screens/tab_navigator/indica_ai/IndicaAiApp.js
+++ mobile/screens/tab_navigator/indica_ai/IndicaAiApp.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducers from './reducers'
-import SearchScreen from './screens/SearchScreen.js'
-
-const store = createStore(rootReducers)
+import RegisterLocal from './containers/RegisterLocal.js'
 
 class IndicaAiApp extends Component {
 
   render() {
 
     return (
-        <Provider store={store}>
-            <SearchScreen />
-       </Provider>
+      <RegisterLocal/>
     );
   }
 }
