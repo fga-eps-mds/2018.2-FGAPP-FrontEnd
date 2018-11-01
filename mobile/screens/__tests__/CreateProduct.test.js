b614ffe30d7c37f3710a660043cd61c9af511b0a
Shayane Alcantara <shayaneliebe@gmail.com>
2018-10-31 22:51:01 -0300

    Adding snapshots in ProductImage,ProductCard and CreateProduct
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

---
+++ mobile/screens/__tests__/CreateProduct.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateProduct from '../tab_navigator/vendas/screens/my_products/CreateProduct';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import renderer from 'react-test-renderer';
+
+Enzyme.configure({adapter: new Adapter()});
+
+it('renders correctly', () => {
+  const tree = renderer.create(<CreateProduct />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ mobile/screens/__tests__/ProductImage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductImage from '../tab_navigator/vendas/components/ProductImage';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import renderer from 'react-test-renderer';
+
+Enzyme.configure({adapter: new Adapter()});
+
+it('renders correctly', () => {
+  const tree = renderer.create(<ProductImage />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
