653c50806a1efe7a22e6a9fc5835eead651af898
Gabriel Davi <gdavi.gd@gmail.com>
2018-11-01 18:47:17 -0300

    fga-eps-mds/2018.2-IndicaAi#262 - Began to create the logic to fetch data into the api
    Co-authored-by: WeillerFernandes <weillerfernandes@gmail.com>

--- mobile/screens/tab_navigator/indica_ai/containers/RegisterLocalAPI.js
+++ mobile/screens/tab_navigator/indica_ai/containers/RegisterLocalAPI.js
@@ -7,9 +7,28 @@ import RegisterAPIForm from '../components/RegisterAPIForm.js'
 
 export default class RegisterLocalAPI extends Component{
 
+ _postForm  = async (jsonForm) => {
+  try{
+    const response = await fetch('#', {method: 'POST', body: JSON.stringify(jsonForm)});
+  	if(response.ok){
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  }catch(error){
+    console.log(error);
+  }
+}
+
+  takeDataFromTheForm = (JsonForm) => {
+    this._postForm (JsonForm);
+  }
+
   render() {
     return (
-      <RegisterAPIForm />
+      <RegisterAPIForm
+      sendDateFromTheForm = {this.takeDataFromTheForm}
+      />
     );
   }
 
