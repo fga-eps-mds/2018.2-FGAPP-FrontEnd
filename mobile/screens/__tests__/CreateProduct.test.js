d3883e2d9ec29b24b62765c57587cdef753e38a1
Shayane Alcantara <shayaneliebe@gmail.com>
2018-11-02 19:34:47 -0300

    Adding open and close dialog test.
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

--- mobile/screens/__tests__/CreateProduct.test.js
+++ mobile/screens/__tests__/CreateProduct.test.js
@@ -12,3 +12,35 @@ it('renders correctly', () => {
   const tree = renderer.create(<CreateProduct navigation={navigation}/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+// test('on change title input', () => {
+//   const navigation = jest.fn();
+//   const value = {
+//     token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1MywidXNlcm5hbWUiOiJ0ZXN0ZTE2QGdtYWlsLmNvbSIsImV4cCI6MTU0MDYwMjA5MSwiZW1haWwiOiJ0ZXN0ZTE2QGdtYWlsLmNvbSIsIm9yaWdfaWF0IjoxNTQwNjAxNzkxfQ.SNTK7ViXyoLH54YBAIgF_uvIBqDetEkMFTNTzkXeUY8",
+//     fk_vendor: 1,
+//     name: 'Product 1',
+//     price: 11.0,
+//     photo: 'http://rene.r.e.pic.centerblog.net/o/67fef2a5.jpg',
+//     description: 'Random description'
+//   }
+//   const wrapper = shallow(<CreateProduct />);
+//   wrapper.setState({value: value});
+//   console.log(wrapper.debug());
+//   const title = wrapper.find('Input').at(0);
+//   title.simulate('changeText','text');
+//   expect(wrapper.state('title')).toBe('text');
+// });
+
+it('test openDialog to be true',() => {
+  const navigation = jest.fn();
+  const wrapper = shallow(<CreateProduct navigation = {navigation}/>);
+  const openDialog = wrapper.instance().openDialog();
+  console.log(openDialog);
+});
+
+it('test closeDialog to be false',() => {
+  const navigation = jest.fn();
+  const wrapper = shallow(<CreateProduct navigation = {navigation}/>);
+  const closeDialog = wrapper.instance().closeDialog();
+  console.log(closeDialog);
+});
