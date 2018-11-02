ffe7bed3485ddd990c850589692a2eb9169d1f6d
Shayane Alcantara <shayaneliebe@gmail.com>
2018-11-02 02:57:31 -0300

    Adding snapshot of ErrorDialog file and adjusting name of ProductCard.
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

--- mobile/screens/__tests__/ErrorDialog.test.js
+++ mobile/screens/__tests__/ErrorDialog.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductCard from '../tab_navigator/vendas/components/ProductCard';
+import ErrorDialog from '../tab_navigator/vendas/screens/my_products/ErrorDialog';
 import Adapter from 'enzyme-adapter-react-16';
 import {shallow} from 'enzyme';
 import Enzyme from 'enzyme';
@@ -8,6 +8,6 @@ import renderer from 'react-test-renderer';
 Enzyme.configure({adapter: new Adapter()});
 
 it('renders correctly', () => {
-  const tree = renderer.create(<ProductCard />).toJSON();
+  const tree = renderer.create(<ErrorDialog />).toJSON();
   expect(tree).toMatchSnapshot();
 });
