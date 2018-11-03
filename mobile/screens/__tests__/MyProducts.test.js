0fbaa9a29b90a70f180989796cba4784c59697a4
Shayane Alcantara <shayaneliebe@gmail.com>
2018-11-03 02:58:45 -0300

    Creating tests to load and refresh userProducts in MyProducts.
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

--- mobile/screens/__tests__/MyProducts.test.js
+++ mobile/screens/__tests__/MyProducts.test.js
@@ -12,3 +12,31 @@ it('renders correctly', () => {
   const tree = renderer.create(<MyProducts navigation={navigation}/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('test loadUserProducts function',() => {
+  const navigation = jest.fn();
+  const wrapper = shallow(<MyProducts navigation = {navigation}/>);
+  const loadUserProducts = wrapper.instance().loadUserProducts();
+});
+
+it('test refreshUserProducts function',() => {
+  const navigation = jest.fn();
+  const wrapper = shallow(<MyProducts navigation = {navigation}/>);
+  const refreshUserProducts = wrapper.instance().refreshUserProducts();
+});
+
+// it('Testing onPress in ProductCard', () => {
+//   const onPressMock = jest.fn();
+//   const wrapper = shallow(<MyProducts onPress = {onPressMock}/>);
+//   const button = wrapper.find('ProductCard').at(0);
+//   button.simulate('press');
+//   expect(onPressMock).toBeCalled();
+// })
+
+// it('Testing onPress in ProductCard', () => {
+//   const onPressMock = jest.fn();
+//   const wrapper = shallow(<MyProducts onPress = {onPressMock}/>);
+//   const something = wrapper.find('Fab').at(0);
+//   something.simulate('press');
+//   expect(onPressMock).toBeCalled();
+// })
