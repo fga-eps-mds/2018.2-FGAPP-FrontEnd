dabfd44a756689d84a1200345c157704265f9c32
Iasmin Mendes <mendesiasmin96@gmail.com>
2018-11-03 09:13:18 -0300

    Add link to terms of use in app

---
+++ mobile/screens/tab_navigator/settings/TermsOfUse.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    Alert,
+    Image,
+} from "react-native";
+import {StackNavigator, createStackNavigator} from 'react-navigation'
+import {PureComponent} from 'react-native';
+import Markdown from 'react-native-markdown-renderer';
+
+class TermsOfUse extends PureComponent {
+
+  render() {
+      return (
+        <Markdown>{
+      );
+  }
+}
+export default Settings;
