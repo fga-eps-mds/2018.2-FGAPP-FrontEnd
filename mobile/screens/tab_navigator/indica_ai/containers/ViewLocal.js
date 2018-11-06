f94338f68b16e2eb4a1c862e28f61a233256f069
lucas <lucasmacielaguiar@gmail.com>
2018-11-06 02:19:24 -0200

    fga-eps-mds/2018.2-IndicaAi#217 - Formating infos in ViewLocal

--- mobile/screens/tab_navigator/indica_ai/containers/ViewLocal.js
+++ mobile/screens/tab_navigator/indica_ai/containers/ViewLocal.js
@@ -13,7 +13,7 @@ import { withNavigation } from 'react-navigation';
 
 width = Dimensions.get('window').width;
 
-class ViewLocal extends Component{
+class ViewLocal extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -31,73 +31,129 @@ class ViewLocal extends Component{
       telephone,
       local_ratings,
     } = this.state.local;
-    
+
     return (
-        <View style={styles.container}>
-          <ScrollView>
-            <Image style={{height: 230, width: width}}
-              source={require('../assets/fga.jpg')}
+      <View style={styles.container}>
+        <ScrollView>
+          <Image style={{ height: 230, width: width }}
+            source={require('../assets/fga.jpg')}
+          />
+
+          <View style={styles.localContainer}>
+            <Text style={styles.localName}>
+              {name}
+            </Text>
+            <Icon style={styles.localHeart}
+              name='ios-heart-outline'
+              color='black'
+              size={30}
             />
-            <View style={styles.localContainer}>
-              <Text style={styles.localName}>
-                {name}
-              </Text>
-              <Icon style={styles.localHeart}
-                name='ios-heart-outline'
-                color='black'
-                size={30}
-              />
-              <View style={styles.hr}></View>
-              <View style={styles.localMap}>
-                <LocalMap
-                  latitude = {latitude}
-                  longitude = {longitude}
-                  name = {name}
-                  description = {description}
-                  />
-              </View>
-              <View style={styles.hr}></View>
-              <Text style={styles.localInfoTitle}>
-                Informações:
-              </Text>
-              <View style={styles.fieldDescription}>
-                <Text style={styles.localInfo}>
-                  {description}
-                </Text>
-              </View>
-              <Icon style={styles.localInfoIcons}
-                name='md-call'
-                color='black'
-                size={25}
-              />
-              <Text style={styles.localInfo}>{telephone}</Text>
-              <Icon style={styles.localInfoIcons}
-                name='md-pin'
-                color='black'
-                size={25}
-              />
-              <Text style={styles.localInfo}>{address}</Text>
+
+            <View style={styles.hr}></View>
+
+            {this.displayLocalMap(latitude, longitude, name, description)}
+
+            <Text style={styles.localInfoTitle}>
+              Informações:
+            </Text>
+
+            {this.displayJsxTelephone(telephone)}
+
+            {this.displayJsxAddress(address)}
+
+            <View style={styles.fieldInfo}>
               <Icon style={styles.localInfoIcons}
                 name='md-clock'
                 color='black'
                 size={25}
               />
               <Text style={styles.localInfo}>6:00 - 22:00</Text>
-              <View style={styles.hr}></View>
-              <Text style={styles.localInfoTitle}>
-                Avaliação:
-              </Text>
-              <Icon style={styles.localInfoIcons}
-                name='md-star'
-                color='black'
-                size={25}
-              />
-                <Text style={styles.localInfo}>4.0</Text>
             </View>
-          </ScrollView>
-        </View>
+
+            <View style={styles.fieldDescription}>
+              {this.displayJsxDescription(description)}
+            </View>
+
+            <View style={styles.hr}></View>
+
+            <Text style={styles.localInfoTitle}>
+              Avaliação:
+            </Text>
+
+            <Icon style={styles.localInfoIcons}
+              name='md-star'
+              color='black'
+              size={25}
+            />
+            <Text style={styles.localInfo}>4.0</Text>
+          </View>
+        </ScrollView>
+      </View>
     );
   }
+
+  displayLocalMap(latitude, longitude, name, description) {
+    if (latitude && longitude) {
+      return (
+        <View>
+          <View style={styles.localMap}>
+            <LocalMap
+              latitude={latitude}
+              longitude={longitude}
+              name={name}
+              description={description}
+            />
+          </View>
+          <View style={styles.hr}></View>
+        </View>
+      );
+    }
+  }
+
+  displayJsxTelephone(info) {
+    if (info) {
+      return (
+        <View style={styles.fieldInfo}>
+          <Icon style={styles.localInfoIcons}
+            name='md-call'
+            color='black'
+            size={25}
+          />
+          <Text style={styles.localInfo}>
+            {info}
+          </Text>
+        </View>
+      );
+    }
+  }
+
+  displayJsxAddress(info) {
+    if (info) {
+      return (
+        <View style={styles.fieldInfo}>
+          <Icon style={styles.localInfoIcons}
+            name='md-pin'
+            color='black'
+            size={25}
+          />
+          <Text style={styles.localInfo}>
+            {info}
+          </Text>
+        </View>
+      );
+    }
+  }
+
+  displayJsxDescription(description) {
+    if (description) {
+      return (
+        <Text style={styles.description}>
+          {description}
+        </Text>
+      );
+    }
+  }
+
 }
 
 export default withNavigation(ViewLocal);
@@ -106,55 +162,66 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
-    position:"absolute",
+    position: "absolute",
     backgroundColor: "white",
-    top:0,
-    bottom:0,
-    left:0,
-    right:0,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
-  localContainer:{
+  localContainer: {
     padding: 10,
   },
-  hr:{
+  hr: {
     borderBottomColor: 'black',
     borderBottomWidth: 1,
     width: 330,
     left: 4.5,
     marginVertical: 10
   },
-  localName:{
+  localName: {
     width: 250,
     fontSize: 20,
     marginTop: 10,
     marginLeft: 15,
     marginBottom: -25,
   },
-  localHeart:{
+  localHeart: {
     marginLeft: 285,
     top: -15
   },
-  localMap:{
+  localMap: {
     height: 180,
     width: 320,
     left: 10
   },
-  localInfoIcons:{
+  localInfoIcons: {
     left: 20
   },
-  localInfoTitle:{
+  localInfoTitle: {
     left: 20,
     fontSize: 18,
     marginBottom: 7
   },
-  localInfo:{
+  localInfo: {
     left: 50,
     top: -22,
-    marginBottom: -10
+    marginBottom: -10,
   },
-  fieldDescription:{
-    marginTop: 20,
+  fieldInfo: {
+    marginTop: 10,
     marginRight: 10,
     width: '80%',
+  },
+  fieldDescription: {
+    marginTop: 30,
+    marginRight: 10,
+    width: '80%',
+  },
+  description: {
+    fontSize: 16,
+    left: 50,
+    top: -22,
+    marginBottom: -10,
   }
 });
