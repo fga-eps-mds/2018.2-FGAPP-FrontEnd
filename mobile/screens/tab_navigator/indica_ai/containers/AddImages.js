078a9f563f0f1897460297dc02762f3a201b22d9
zarbielli <jlfz06@gmail.com>
2018-11-09 16:32:42 -0200

    fga-eps-mds/2018.2-IndicaAi#265 - Adding fucntion to pick images from gallery

--- mobile/screens/tab_navigator/indica_ai/containers/AddImages.js
+++ mobile/screens/tab_navigator/indica_ai/containers/AddImages.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
-import {
-  View,
-  TouchableOpacity
-} from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
+import { ImagePicker } from 'expo';
+import { SERVER_URI, PostFunStuff } from '../../constant';
+import PropTypes from 'prop-types';
 
 
 class AddImages extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+    this.pickImage = this.pickImage.bind(this);
+    this.postImage = this.postImage.bind(this);
+  }
+
+  pickImage = async () => {
+    const result = await ImagePicker.lauchImageLibraryAsync({
+      allowsEditing: true,
+      base64: true,
+    });
+
+    if(!result.cancelled) { 
+      this.setState({
+        image: result.uri,
+      });
+    } 
+  };  
+  
   render() {
     return (
       <TouchableOpacity>
