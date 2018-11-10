3c4517ddf6cbc590a567852ae082b42f5b548585
zarbielli <jlfz06@gmail.com>
2018-11-10 10:22:43 -0200

    fga-eps-mds/2018.2-IndicaAi#202 - Adding ImageModal
    Co-authored-by: ridersk <lucasmacielaguiar@gmail.com>

---
+++ mobile/screens/tab_navigator/indica_ai/components/ImageModal.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+
+export default class ImageModal extends React.Component {
+
+  render() {
+    return (
+      <Modal animationType="fade" transparent={true} visible={this.props.visible} onRequestClose={() => null}>
+        <View style={styles.modalContainer}>
+
+          <View style={styles.boxContainer}>
+            <Text style={styles.boxTitle}>Enviar foto ?</Text>
+            <View style={styles.buttonContainer}>
+
+              <TouchableOpacity style={[styles.button, styles.sendButton]} onPress={this.props.onCancel}>
+                <Text style={styles.buttonText}>Enviar</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+
+            </View>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxContainer: {
+    padding: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    alignItems: 'center',
+    width: 280,
+  },
+  boxTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginTop: 8
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 40,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    borderRadius: 3,
+  },
+  sendButton: {
+    backgroundColor: 'green',
+  },
+  cancelButton: {
+    backgroundColor: 'red',
+    marginLeft: 10,
+
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 12,
+  },
+
+});
