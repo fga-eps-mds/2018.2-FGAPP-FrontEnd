7cf9f4bb1f163138d9925d987a9b77ad439b4146
Shayane Alcântara <shayaneliebe@gmail.com>
2018-11-13 21:22:00 -0200

    HotFix: removing comment

--- mobile/screens/__tests__/MyProductDetails.test.js
+++ mobile/screens/__tests__/MyProductDetails.test.js
@@ -179,28 +179,3 @@ describe('Test MyProductDetails', () => {
     description.simulate('changeText','text');
     expect(wrapper.state('description')).toBe('text');
   });
-
-
-})
-/* test('change input title ', () => {
-  const wrapper = shallow(<MyProductDetails />);
-  const title = wrapper.find('InputLab').at(0);
-  title.simulate('changeText','text');
-  expect(wrapper.state('title')).toBe('text');
-});
-
-test('change input price ', () => {
-  const wrapper = shallow(<MyProductDetails />);
-  const price = wrapper.find('InputLab').at(1);
-  price.simulate('changeText','text');
-  expect(wrapper.state('price')).toBe('text');
-});
-
-test('change text area description ', () => {
-  const wrapper = shallow(<MyProductDetails />);
-  //console.log(wrapper.debug());
-  const description = wrapper.find('Styled(Textarea)').at(0);
-  description.simulate('changeText','text');
-  expect(wrapper.state('description')).toBe('text');
-});
-*/
