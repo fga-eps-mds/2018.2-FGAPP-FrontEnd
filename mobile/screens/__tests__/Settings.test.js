58fcc5d39f1e608d2055ed03ea35bf3a4e3185bd
Dâmaso Júnio P. Brasileo <junio.pereirab@gmail.com>
2018-11-13 23:49:18 -0200

    Doing settings screen tests
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

---
+++ mobile/screens/__tests__/Settings.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Settings from '../tab_navigator/settings/settings/Settings';
+import Adapter from 'enzyme-adapter-react-16';
+//import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import renderer from 'react-test-renderer';
+//import fetchMock from 'fetch-mock';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('Test Settings', () => {
+    const navigation = {
+        state: {
+            params: {
+                token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InJvZ2VybGVua2VAZ21haWwuY29tIiwidXNlcl9pZCI6MSwib3JpZ19pYXQiOjE1NDE3MTk3NDksImV4cCI6MTU0MTcyMDA0OSwidXNlcm5hbWUiOiJyb2dlcmxlbmtlQGdtYWlsLmNvbSJ9.eCEGRB9yYAkP5iBIybeDsAoWk4HyusPUTX3LBiP0I64",
+            }
+        },
+        navigate: jest.fn(),
+    }
+
+    it('renders correctly', () => {
+        const tree = renderer.create(<Settings navigation = {navigation}/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+})
