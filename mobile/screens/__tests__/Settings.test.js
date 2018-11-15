ecbf35366917cdf1236baca720b640dc3c7b6587
Welison Lucas <welison.almeida.923@gmail.com>
2018-11-15 15:14:39 -0200

    Test the loadProfile function completely.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

--- mobile/screens/__tests__/Settings.test.js
+++ mobile/screens/__tests__/Settings.test.js
@@ -6,14 +6,21 @@ import Enzyme from 'enzyme';
 import renderer from 'react-test-renderer';
 import fetchMock from 'fetch-mock';
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Test Settings', () => {
     const navigation = {
         state: {
             params: {
-                token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InJvZ2VybGVua2VAZ21haWwuY29tIiwidXNlcl9pZCI6MSwib3JpZ19pYXQiOjE1NDE3MTk3NDksImV4cCI6MTU0MTcyMDA0OSwidXNlcm5hbWUiOiJyb2dlcmxlbmtlQGdtYWlsLmNvbSJ9.eCEGRB9yYAkP5iBIybeDsAoWk4HyusPUTX3LBiP0I64",
+                token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InJvZ2VybGVua2VAZ21haWwuY29tIiwidXNlcl9pZCI6MSwib3JpZ19pYXQiOjE1NDE3MTk3NDksImV4cCI6MTU0MTcyMDA0OSwidXNlcm5hbWUiOiJyb2dlcmxlbmtlQGdtYWlsLmNvbSJ9.eCEGRB9yYAkP5iBIybeDsAoWk4HyusPUTX3LBiP0I64',
             }
+        },
+        navigate: jest.fn(),
+    }
+
+    const navigation2 = {
+        state: {
+
         },
         navigate: jest.fn(),
     }
@@ -28,17 +35,22 @@ describe('Test Settings', () => {
     });
 
     it('renders correctly', () => {
-        const tree = renderer.create(<Settings navigation = {navigation}/>).toJSON();
+        const tree = renderer.create(<Settings navigation={navigation} />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
-    it('Test _loadProfile fetch with sucess', async (done) => {
-        const wrapper = shallow(<Settings navigation = {navigation}/>);
+    it('renders correctly without token', () => {
+        const wrapper = shallow(<Settings navigation={navigation2} />);
+    });
 
+    it('Test _loadProfile fetch with sucess', async (done) => {
+        const wrapper = shallow(<Settings navigation={navigation} />);
         const state = {
-            name: 'Naruto Uzumaki',
-            email: 'uzumakinaruto@gmail.com',
-            photo: 'https://res-console.cloudinary.com/integraappfga/thumbnails/v1/image/upload/v1541537829/c2VuazJvZG54YW1vcHdsa215b3E=/grid'
+            profileInfo: {
+                name: 'Naruto Uzumaki',
+                email: 'uzumakinaruto@gmail.com',
+                photo: 'https://res-console.cloudinary.com/integraappfga/thumbnails/v1/image/upload/v1541537829/c2VuazJvZG54YW1vcHdsa215b3E=/grid'
+            }
         }
 
         fetchMock.post(profileInfoPath, [
@@ -48,17 +60,17 @@ describe('Test Settings', () => {
                 photo: 'https://res-console.cloudinary.com/integraappfga/thumbnails/v1/image/upload/v1541537829/c2VuazJvZG54YW1vcHdsa215b3E=/grid',
             }
         ]);
-        
+
         wrapper.setState(state);
         await wrapper.instance()._loadProfile();
-    
+
         process.nextTick(() => {
             done();
         });
     });
 
     it('Test _loadProfile fetch with error response', async (done) => {
-        const wrapper = shallow(<Settings navigation = {navigation}/>);
+        const wrapper = shallow(<Settings navigation={navigation} />);
 
         const state = {
             name: '',
@@ -70,14 +82,13 @@ describe('Test Settings', () => {
             "error": "This field is required."
         };
 
-        fetchMock.post(profileInfoPath, {error});
-        
+        fetchMock.post(profileInfoPath, { error });
+
         wrapper.setState(state);
         await wrapper.instance()._loadProfile();
-    
+
         process.nextTick(() => {
             done();
         });
     });
-
-})
+});
