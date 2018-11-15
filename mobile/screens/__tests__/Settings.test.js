ef7cdc511964eb2cd430088c647a54ef504543c8
Welison Lucas <welison.almeida.923@gmail.com>
2018-11-15 14:09:57 -0200

    Test fetch function of Settings screen.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>

--- mobile/screens/__tests__/Settings.test.js
+++ mobile/screens/__tests__/Settings.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Settings from '../tab_navigator/settings/settings/Settings';
 import Adapter from 'enzyme-adapter-react-16';
-//import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import renderer from 'react-test-renderer';
-//import fetchMock from 'fetch-mock';
+import fetchMock from 'fetch-mock';
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -18,8 +18,66 @@ describe('Test Settings', () => {
         navigate: jest.fn(),
     }
 
+    beforeAll(() => {
+        process.env.INTEGRA_LOGIN_AUTH = 'http://test.ip';
+        profileInfoPath = `${process.env.INTEGRA_LOGIN_AUTH}/api/users/get_profile/`;
+    });
+
+    beforeEach(() => {
+        fetchMock.restore();
+    });
+
     it('renders correctly', () => {
         const tree = renderer.create(<Settings navigation = {navigation}/>).toJSON();
         expect(tree).toMatchSnapshot();
     });
+
+    it('Test _loadProfile fetch with sucess', async (done) => {
+        const wrapper = shallow(<Settings navigation = {navigation}/>);
+
+        const state = {
+            name: 'Naruto Uzumaki',
+            email: 'uzumakinaruto@gmail.com',
+            photo: 'https://res-console.cloudinary.com/integraappfga/thumbnails/v1/image/upload/v1541537829/c2VuazJvZG54YW1vcHdsa215b3E=/grid'
+        }
+
+        fetchMock.post(profileInfoPath, [
+            {
+                name: 'Naruto Uzumaki',
+                email: 'uzumakinaruto@gmail.com',
+                photo: 'https://res-console.cloudinary.com/integraappfga/thumbnails/v1/image/upload/v1541537829/c2VuazJvZG54YW1vcHdsa215b3E=/grid',
+            }
+        ]);
+        
+        wrapper.setState(state);
+        await wrapper.instance()._loadProfile();
+    
+        process.nextTick(() => {
+            done();
+        });
+    });
+
+    it('Test _loadProfile fetch with error response', async (done) => {
+        const wrapper = shallow(<Settings navigation = {navigation}/>);
+
+        const state = {
+            name: '',
+            email: '',
+            photo: 'https://res-console.cloudinary.com/integraappfga/thumbnails/v1/image/upload/v1541537829/c2VuazJvZG54YW1vcHdsa215b3E=/grid'
+        }
+
+        const error = {
+            "error": "This field is required."
+        };
+
+        fetchMock.post(profileInfoPath, {error});
+        
+        wrapper.setState(state);
+        await wrapper.instance()._loadProfile();
+    
+        process.nextTick(() => {
+            done();
+        });
+    });
+
 })
