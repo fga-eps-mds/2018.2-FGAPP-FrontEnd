f8c053e349045b30aa9130f370f2d88383743167
Weiller Fernandes <weillerfernandes@gmail.com>
2018-11-21 10:02:54 -0200

    fga-eps-mds/2018.2-IndicaAi#293 - Doing snapshot test in Local component
    Co-authored-by: Renatovga2602 <renatovalerio25@gmail.com>

--- mobile/screens/tab_navigator/indica_ai/__tests__/Local.test.js
+++ mobile/screens/tab_navigator/indica_ai/__tests__/Local.test.js
@@ -4,11 +4,11 @@ import {shallow} from 'enzyme';
 import Enzyme from 'enzyme';
 import renderer from 'react-test-renderer';
 
-import ButtonWithIcon from '../components/ButtonWithIcon';
+import Local from '../components/Local';
 
 Enzyme.configure({adapter: new Adapter()});
 
   it('renders correctly', () => {
-    const tree = renderer.create(<ButtonWithIcon />).toJSON();
+    const tree = renderer.create(<Local />).toJSON();
     expect(tree).toMatchSnapshot();
 });
