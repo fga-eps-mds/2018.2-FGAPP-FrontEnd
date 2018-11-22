859dbf92406d90ae2bab6349dd60bfecd1723a34
gleal17 <guilhermelml@gmail.com>
2018-11-22 16:09:36 -0200

    fga-eps-mds/2018.2-Roles#147 Removed mobile/env-config.js

--- mobile/env-config.js
+++
@@ -1,3 +0,0 @@
-process.env.VENDAS_API='https://integra-vendas-api-gateway.herokuapp.com'
-process.env.INTEGRA_LOGIN_AUTH='https://integra-vendas-login.herokuapp.com'
-process.env.INDICA_AI_API='https://indicaai.herokuapp.com'
