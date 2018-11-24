aef5704f884f5ee42b41b67e4bb1893b151d7110
Shayane Alcantara <shayaneliebe@gmail.com>
2018-11-24 01:58:55 -0200

    Fixing field test.
    Co-authored-by: Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>

--- mobile/screens/__tests__/Vendas/components/Field.test.js
+++ mobile/screens/__tests__/Vendas/components/Field.test.js
@@ -7,7 +7,24 @@ import renderer from 'react-test-renderer';
 
 Enzyme.configure({adapter: new Adapter()});
 
+describe('Testing Field', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Field />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  jest.mock('Animated', () => {
+  const ActualAnimated = require.requireActual('Animated')
+    return {
+      ...ActualAnimated,
+      timing: (value, config) => ({
+        start: callback => {
+          value.setValue(config.toValue)
+          if (callback) {
+            callback()
+          }
+        },
+      }),
+    }
+  })
+})
