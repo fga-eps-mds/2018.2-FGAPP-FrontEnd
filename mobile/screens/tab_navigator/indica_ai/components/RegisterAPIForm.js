c1d108692c24a510c8a906fda2c0443f94daa054
Heron Rodrigues <heronrsousa@hotmail.com>
2018-11-23 23:58:51 -0200

    fga-eps-mds/2018.2-IndicaAi#294 - Fixing the keyboard problem in register screen

--- mobile/screens/tab_navigator/indica_ai/components/RegisterAPIForm.js
+++ mobile/screens/tab_navigator/indica_ai/components/RegisterAPIForm.js
@@ -3,22 +3,14 @@ import {
   View,
   Text,
   StyleSheet,
-  Image,
-  ScrollView,
   TextInput,
-  Alert,
   TouchableOpacity
 } from "react-native";
 import {
-  Container,
-  Form,
-  Item,
-  Input,
-  Label,
-  Picker,
-  Textarea,
-  Button
-} from 'native-base'
+  Button,
+  Container
+} from 'native-base';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import CategorySelect from './CategorySelect.js';
 import HoursSelect from './HoursSelect.js';
 import { withNavigation } from 'react-navigation';
@@ -47,204 +39,44 @@ export default class RegisterAPIForm extends Component {
 
   render() {
     return (
-      <Container style={styles.container}>
-        <CategorySelect
-          setSelectedCategories={this.props.setSelectedCategories}
-        />
+      <KeyboardAwareScrollView style={{width: '100%', padding: 20}}>
+        <Container>
 
-        <Item
-          style={styles.pickerForm}
-          regular
-        >
-          <Input placeholder='Nome'
-            onChangeText={(name) => this.setState({ name })}
-          />
-        </Item>
+            <TextInput
+              style={styles.inputName}
+              placeholder='Nome'
+              placeholderTextColor='black'
+              underlineColorAndroid='transparent'
+              autoCapitalize='none'
+              onChangeText={(name) => this.setState({ name })}
+            />
 
-        <TextInput
-          editable = {true}
-          maxLength = {150}
-          numberOfLines = {5}
-          placeholder="Adicione uma descrição do local"
-          placeholderTextColor='black'
-          onChangeText={(description) => this.setState({ description })} />
-
-        <View style={styles.hoursForm}>
-          <Text style={styles.hoursFormTitle}>Horario de funcionamento:</Text>
-          <View style={styles.hoursOption}>
-            <TouchableOpacity onPress={() => {this.setState({eachDay: true, week: false})}}>
-              <Text style={styles.hoursFormOption}>Diario</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {this.setState({week: true, eachDay: false})}}>
-              <Text style={styles.hoursFormOption}>Semanal</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        {this.state.eachDay ? this.displayHoursEachDay() : null}
-        {this.state.week ? this.displayHoursWeek() : null}
-
-        <View style={styles.button}>
-          <Button block info onPress={
-            () => {
-              if (!(this.state.name)) {
-                Alert.alert(
-                  'Atenção!',
-                  "Os campos 'Nome' ou 'Descrição' não podem estar vazios",
-                  [
-                    { text: 'OK', onPress: () => console.log('OK Pressed') }
-                  ],
-                  { cancelable: false }
-                )
-              } else {
-                this.props.sendDataToTheForm(this.state.name, this.state.description)
-              }
-            }
-
-          }>
-          <Text style={{ color: "white" }}>Confirmar</Text>
-          </Button>
-        </View>
-        <WarningModal
-          onCancel={() => this.setState({ errorModalVisible: false })}
-          visible={this.state.errorModalVisible}
-          message = {"Erro no nome ou descrição"}
-        />
-      </Container>
+        </Container>
+      </KeyboardAwareScrollView>
     );
 
   }
-
-  displayHoursEachDay() {
-
-    const days = [{id: 1, day: 'Dom'}, {id: 2, day: 'Seg'}, {id: 3, day: 'Ter'}, {id: 4, day: 'Qua'}, {id: 5, day: 'Qui'}, {id: 6, day: 'Sex'}, {id: 7, day: 'Sab'}];
-
-    return(
-      <View style={{top: 20}}>
-        <View style={styles.hoursForm}>
-          {days.map( day =>
-            <View
-              style={styles.dayBorder}
-              key={day.id}
-            >
-              <Text style={styles.day}>
-                {day.day}
-              </Text>
-            </View>
-          )}
-        </View>
-        <View style={styles.hoursForm}>
-          {days.map( day =>
-            <HoursSelect
-              option='Abre'
-              day={day.id}
-              takeOpeningHours={this.props.takeOpeningHours}
-              key={day.id}
-            />
-          )}
-        </View>
-        <View style={styles.hoursForm}>
-          {days.map( day =>
-            <HoursSelect
-              option='Fecha'
-              day={day.id}
-              takeOpeningHours={this.props.takeOpeningHours}
-              key={day.id}
-            />
-          )}
-        </View>
-      </View>
-    );
-  }
-
-  displayHoursWeek() {
-
-    const days = [{id: 1, day: 'Seg a Sex'}, {id: 2, day: 'Sab e Dom'}];
-
-    return(
-      <View style={{top: 20}}>
-        <View style={styles.hoursForm}>
-          {days.map( day =>
-            <View
-              style={styles.dayBorder}
-              key={day.id}
-            >
-              <Text style={styles.day}>
-                {day.day}
-              </Text>
-            </View>
-          )}
-        </View>
-        <View style={styles.hoursForm}>
-          {days.map( day =>
-            <HoursSelect
-              option='Abre'
-              week={true}
-              day={day.id}
-              takeOpeningHours={this.props.takeOpeningHours}
-              key={day.id}
-            />
-          )}
-        </View>
-        <View style={styles.hoursForm}>
-          {days.map( day =>
-            <HoursSelect
-              option='Fecha'
-              week={true}
-              day={day.id}
-              takeOpeningHours={this.props.takeOpeningHours}
-              key={day.id}
-            />
-          )}
-        </View>
-      </View>
-    );
-  }
-
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
     backgroundColor: "white",
-    padding: 20,
+    position: 'absolute',
+    padding: 15,
+    flex: 1,
     left: 0,
-    right: 0,
-    flex: 1
+    right: 0
   },
-  pickerForm: {
-    top: 65,
-    marginBottom: 74
+  inputName: {
+    height: 40,
+    fontSize: 15,
+    borderWidth: 1,
+    borderRadius: 5
   },
-  button: {
-    top: 50,
-    padding: 10,
-  },
-  hoursOption:{
-    flexDirection: 'row'
-  },
-  hoursForm: {
+  inputDescription: {
     top: 20,
-    flexDirection: 'row'
-  },
-  dayBorder: {
+    height: 55,
     borderWidth: 1,
-    flex: 1
-  },
-  hoursFormOption: {
-    borderWidth: 1,
-    borderRadius: 2,
-    padding: 3,
-    marginLeft: 5
-  },
-  day: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  hoursFormTitle: {
-    fontSize: 15,
-    fontWeight: 'bold'
+    borderRadius: 5
   }
 });
