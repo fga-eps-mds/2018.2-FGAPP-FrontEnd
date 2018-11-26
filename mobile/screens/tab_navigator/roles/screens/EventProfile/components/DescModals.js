e126e1ecb576a58840e045fa0462b4174b382c3f
Victor Rodrigues <manovictor5@gmail.com>
2018-11-26 00:47:55 -0200

    fga-eps-mds/2018.2-Roles#165 Retirando erros do descModals
    Co-authored-by: VictorRodriguesS0 <manovictor5@gmail.com>
    Co-authored-by: gleal17 <guilhermelml@gmail.com>

--- mobile/screens/tab_navigator/roles/screens/EventProfile/components/DescModals.js
+++ mobile/screens/tab_navigator/roles/screens/EventProfile/components/DescModals.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import { Card, CardItem, Body, Icon, Button } from "native-base"
+import React, { Component } from "react";
+import { Card, CardItem, Body, Icon, Button } from "native-base";
 import {
 	ScrollView,
 	Text,
@@ -8,16 +8,21 @@ import {
 	Linking,
 	View,
 	TouchableHighlight
-} from "react-native"
-import Modal from "react-native-modal"
+} from "react-native";
+import Modal from "react-native-modal";
 
 class DescModals extends Component {
 	_gotoURL = () => {
-		Linking.openURL(this.props.refURL)
-	}
+		Linking.openURL(this.props.refURL);
+	};
 
 	render() {
-		const alturaDinamica = this.props.text.length > 200 ? '60%':'30%'
+		let alturaDinamica;
+		if (this.props.text == undefined) {
+			alturaDinamica = "60%";
+		} else {
+			alturaDinamica = this.props.text.length > 200 ? "60%" : "30%";
+		}
 		return (
 			<Modal
 				isVisible={this.props.visible}
@@ -27,14 +32,21 @@ class DescModals extends Component {
 				animationOut="fadeOutDown"
 			>
 				<Card style={styles.modalCard}>
-					<CardItem
-						header
-						bordered
-						style={{ height: 50, alignSelf: "center" }}
-					>
-						<Icon name={this.props.icon} style={this.props.color == null ? {color:'black'} : {color:this.props.color}}/>
-						<Text 
-							style={this.props.color == null ? {color:'black'} : {color:this.props.color}}
+					<CardItem header bordered style={{ height: 50, alignSelf: "center" }}>
+						<Icon
+							name={this.props.icon}
+							style={
+								this.props.color == null
+									? { color: "black" }
+									: { color: this.props.color }
+							}
+						/>
+						<Text
+							style={
+								this.props.color == null
+									? { color: "black" }
+									: { color: this.props.color }
+							}
 						>
 							{this.props.headerLabel}
 						</Text>
@@ -43,38 +55,34 @@ class DescModals extends Component {
 					<ScrollView
 						style={{
 							height: alturaDinamica,
-							backgroundColor: "#f2f2f2",
+							backgroundColor: "#f2f2f2"
 						}}
 					>
-						<Text style={{ textAlign: "center"}}>
+						<Text style={{ textAlign: "center" }}>
 							{"\n" + this.props.text + "\n"}
 						</Text>
 					</ScrollView>
 
-					
-					{this.props.link != null && 
+					{this.props.link != null && (
 						<CardItem style={{ alignSelf: "center" }}>
 							<Icon name="link" />
 							<TouchableOpacity onPress={this._gotoURL}>
-								<Text style={{ color: "blue" }}>
-									{this.props.link}
-								</Text>
+								<Text style={{ color: "blue" }}>{this.props.link}</Text>
 							</TouchableOpacity>
 						</CardItem>
-					}
-
+					)}
 				</Card>
 			</Modal>
-		)
+		);
 	}
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
 	modalCard: {
 		width: "90%",
 		alignSelf: "center",
 		borderRadius: 10
 	}
-})
+});
 
-export default DescModals
+export default DescModals;
