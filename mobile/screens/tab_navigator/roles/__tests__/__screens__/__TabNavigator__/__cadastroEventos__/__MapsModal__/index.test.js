51bae19d0a7e755f43f3c01920db9fc977c71198
gleal17 <guilhermelml@gmail.com>
2018-11-26 00:53:58 -0200

    fga-eps-mds/2018.2-Roles#165 Retirando erros do MapsModal
    Co-authored-by: gleal17 <guilhermelml@gmail.com>
    Co-authored-by: VictorRodriguesS0 <manovictor5@gmail.com>

--- mobile/screens/tab_navigator/roles/__tests__/__screens__/__TabNavigator__/__cadastroEventos__/__MapsModal__/index.test.js
+++ mobile/screens/tab_navigator/roles/__tests__/__screens__/__TabNavigator__/__cadastroEventos__/__MapsModal__/index.test.js
@@ -11,3 +11,33 @@ it('renders correctly', () => {
   const tree = renderer.create(<MapsModal/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+jest.mock('react-native-maps', () => {                                           
+  const React = require.requireActual('react');                                  
+  const MapView = require.requireActual('react-native-maps');                    
+                                                                                 
+  class MockCallout extends React.Component {                                    
+    render() {                                                                   
+      return React.createElement('Callout', this.props, this.props.children);    
+    }                                                                            
+  }                                                                              
+                                                                                 
+  class MockMarker extends React.Component {                                     
+    render() {                                                                   
+      return React.createElement('Marker', this.props, this.props.children);     
+    }                                                                            
+  }                                                                              
+                                                                                 
+  class MockMapView extends React.Component {                                    
+    render() {                                                                   
+      return React.createElement('MapView', this.props, this.props.children);    
+    }                                                                            
+  }                                                                              
+                                                                                 
+  MockCallout.propTypes = MapView.Callout.propTypes;                             
+  MockMarker.propTypes = MapView.Marker.propTypes;                               
+  MockMapView.propTypes = MapView.propTypes;                                     
+  MockMapView.Marker = MockMarker;                                               
+  MockMapView.Callout = MockCallout;                                             
+  return MockMapView;                                                            
+}); 
